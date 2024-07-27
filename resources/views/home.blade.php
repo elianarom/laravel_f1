84a7982d5f8606213196438c87f7ef8e94882a64
elianarom <168400508+elianarom@users.noreply.github.com>
2024-07-27 11:24:41 -0300

    suscripciones en home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -56,6 +56,33 @@
         </div>
       </header>
 
+      <section class="relative mx-auto mt-32 w-full max-w-container px-4 sm:px-6 lg:px-8">
+        <div class="flex flex-col lg:mx-32">
+            <div class="md:text-center">
+                <h1 class="font-display text-3xl tracking-tight text-black sm:text-4xl">Planes de Suscripción</h1>
+                <p class="mt-4 text-lg text-slate-600">Elegí el plan de suscripción que querés contratar.</p>
+            </div>
 
+            <div class="-mx-4 mt-16 mb-20 grid max-w-2xl grid-cols-1 gap-y-10 sm:mx-auto lg:-mx-8 lg:max-w-none lg:grid-cols-3 xl:mx-0 xl:gap-x-8">
+
+                <div class="flex flex-col ml-5 mr-5 rounded-3xl px-6 sm:px-10 lg:py-16 bg-red-600 hover:bg-black">
+                    <h2 class="mt-5 mb-10 font-bold text-3xl text-white">BASIC</h2>
+                    <p class="order-first font-display text-5xl font-bold tracking-tight text-white"><span class="text-light text-2xl">$</span> 0</p>
+                    <a href="{{ route('auth.loginForm') }}" class="flex items-center justify-between py-2.5 px-6 rounded-full bg-white text-nlack text-sm lg:text-base font-bold  transition-all duration-500 mb-2 lg:mr-6 md:mb-0 md:mr-3">Suscribirme</a>
+                </div>
+                <div class="flex flex-col ml-5 mr-5 rounded-3xl px-6 sm:px-10 lg:py-16 bg-red-600 hover:bg-black">
+                    <h2 class="mt-5 mb-10 font-bold text-3xl text-white">MEDIUM</h2>
+                    <p class="order-first font-display text-5xl font-bold tracking-tight text-white"><span class="text-light text-2xl">$</span> 5</p>
+                    <a href="{{ route('auth.loginForm') }}" class="flex items-center justify-between py-2.5 px-6 rounded-full bg-white text-nlack text-sm lg:text-base font-bold  transition-all duration-500 mb-2 lg:mr-6 md:mb-0 md:mr-3">Suscribirme</a>
+                </div>
+                <div class="flex flex-col ml-5 mr-5 rounded-3xl px-6 sm:px-10 lg:py-16 bg-red-600 hover:bg-black">
+                    <h2 class="mt-5 mb-10 font-bold text-3xl text-white">GOLD</h2>
+                    <p class="order-first font-display text-5xl font-bold tracking-tight text-white"><span class="text-light text-2xl">$</span> 10</p>
+                    <a href="{{ route('auth.loginForm') }}" class="flex items-center justify-between py-2.5 px-6 rounded-full bg-white text-nlack text-sm lg:text-base font-bold  transition-all duration-500 mb-2 lg:mr-6 md:mb-0 md:mr-3">Suscribirme</a>
+                </div>
+
+            </div>
+
+    </section>
 
 </x-layout>
